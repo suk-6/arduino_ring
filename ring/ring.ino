baa8ec1a17ff14f7417372b2509216ea8c9a8f94
Woosuk Nam <me@suk.kr>
2024-03-17 14:43:52 +0900

    Update ring.ino

--- ring/ring.ino
+++ ring/ring.ino
@@ -1,62 +1,63 @@
 #include <SoftwareSerial.h>
 
-#define BT_RXD 8
+#define LEFT 8
+#define RIGHT 9
+#define CENTER 10
+
+#define BT_RXD 6
 #define BT_TXD 7
 SoftwareSerial bluetooth(BT_RXD, BT_TXD);
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   Serial.println("Start");
   bluetooth.begin(9600);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+  pinMode(LEFT, OUTPUT);
+  pinMode(RIGHT, OUTPUT);
+  pinMode(CENTER, OUTPUT);
 }
 
-void loop()
-{
-  if (bluetooth.available())
-  {
+void loop() {
+  if (bluetooth.available()) {
     // 문자열을 읽고 저장
     String readData = bluetooth.readStringUntil('\n');
-    Serial.println(readData); // 읽은 데이터를 시리얼 모니터에 출력
 
     int separatorIndex = readData.indexOf('/');
-    if (separatorIndex != -1)
-    {
+    if (separatorIndex != -1) {
       // '/' 문자를 찾으면 분할
       String positionStr = readData.substring(0, separatorIndex);
       String powerStr = readData.substring(separatorIndex + 1);
 
       int position = positionStr.toInt();
       int power = powerStr.toInt();
+      Serial.println(position);
+      Serial.println(power);
 
-      if (position == 0)
-      {
-        Serial.println(power);
-        analogWrite(4, power);
-        delay(500);
-        analogWrite(4, 0);
+      switch(position){
+        case 0:
+          analogWrite(CENTER, power);
+          analogWrite(LEFT, power);
+          break;
+        case 1:
+          analogWrite(CENTER, power);
+          analogWrite(RIGHT, power);
+          break;
+        case 2:
+          analogWrite(LEFT, power);
+          break;
+        case 3:
+          analogWrite(RIGHT, power);
+          break;
       }
-      else if (position == 1)
-      {
-        analogWrite(5, power);
-        delay(500);
-        analogWrite(5, 0);
-      }
-      else if (position == 2)
-      {
-        analogWrite(4, power);
-        analogWrite(5, power);
-        delay(500);
-        analogWrite(4, 0);
-        analogWrite(5, 0);
-      }
+
+      delay(500);
+      analogWrite(LEFT, 0);
+      analogWrite(RIGHT, 0);
+      analogWrite(CENTER, 0);
     }
   }
 
-  if (Serial.available())
-  {
+  if (Serial.available()) {
     bluetooth.write(Serial.read());
   }
 }
